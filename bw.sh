6fb4887aa5c112cc945eaeddfb1dd6e1ced1f0ee
Aroins <i@tecat.cn>
2019-10-23 19:09:26 +0800

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /www/server
+cd /www/server/
 rm -rf /www/server/panel/static /www/server/panel/templates
 wget -N --no-check-certificate https://github.com/moeik/BT_Panel_Theme/releases/download/BT_Panel_%E7%BC%A4%E7%BA%B7/panel.zip
 unzip -o panel.zip
