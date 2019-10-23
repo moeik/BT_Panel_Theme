0c89257ccb14ab30edcbbb564e0fa2ec22932c3c
Aroins <i@tecat.cn>
2019-10-23 19:03:00 +0800

    Delete bw.sh

--- bw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /www/server
-rm -rf /www/server/panel/static /www/server/panel/templates
-wget https://github.com/moeik/BT_Panel_Theme/releases/download/BT_Panel_%E7%BC%A4%E7%BA%B7/panel.zip
-unzip panel.zip
-/etc/init.d/nginx restart
-echo '安装完成，请清除浏览器缓存再访问！博客地址：aroins.com'
