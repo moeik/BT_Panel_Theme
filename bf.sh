2ae9173791e8e8be983e060beb65b2ef52b7af6c
Aroins <i@tecat.cn>
2019-10-23 19:45:31 +0800

    Update bf.sh

--- bf.sh
+++ bf.sh
@@ -4,4 +4,7 @@ rm -rf /www/server/panel/static /www/server/panel/templates;
 wget -N --no-check-certificate https://github.com/moeik/BT_Panel_Theme/releases/download/BT_Panel_%E7%BC%A4%E7%BA%B7/panel.zip;
 unzip -o panel.zip && /etc/init.d/nginx restart;
 rm -rf /www/server/panel.zip;
-echo '安装完成，请清除浏览器缓存再访问！博客地址：aroins.com'
+echo '
+#********************************************************
+# 安装完成，请清除浏览器缓存再访问！博客地址：aroins.com
+#********************************************************'
