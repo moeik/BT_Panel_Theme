30520af7b37ddacecfa4d48c5dc65b10e49959e6
Aroins <i@tecat.cn>
2019-11-27 13:40:39 +0800

    Create rb.sh

---
+++ rb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd /www/server/;
+rm -rf /www/server/panel/static /www/server/panel/templates;
+wget -N --no-check-certificate https://github.com/moeik/BT_Panel_Theme/releases/download/BT_Panel_%E7%83%AD%E5%B7%B4/panel.zip;
+unzip -o panel.zip;
+/etc/init.d/nginx restart;
+rm -rf /www/server/panel.zip;
+echo '
+#=================================================
+#       主题安装完成
+#       请清除浏览器缓存再访问
+#       Version: 1.0
+#       Author: biezhi
+#       Blog: https://aroins.com
+#=================================================
+'
