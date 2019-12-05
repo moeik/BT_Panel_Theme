3ad6b3c9936abe222f8e90662e03f9aa744c2c69
Aroins <i@tecat.cn>
2019-12-05 15:28:00 +0800

    Update bf.sh

--- bf.sh
+++ bf.sh
@@ -3,7 +3,7 @@ cd /www/server/;
 rm -rf /www/server/panel/static /www/server/panel/templates;
 wget -N --no-check-certificate https://github.com/moeik/BT_Panel_Theme/releases/download/BT_Panel_%E7%BC%A4%E7%BA%B7/panel.zip;
 unzip -o panel.zip;
-/etc/init.d/nginx restart;
+/etc/init.d/bt restart;
 rm -rf /www/server/panel.zip;
 echo '
 #=================================================
